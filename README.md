f3f479c6b30495e326da420e4f41173bf9fa38e2
Jorge Arias <mail@jorgearias.cl>
2017-03-10 23:53:57 -0300

    Change process for run and save volume in /data
    Fixed error when run new container with an existent volume
    Closed #4

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 ```sh
 docker run --detach --name tuleap \
   -p 80:80 -p 443:443 \
-  --env DEFAULT_DOMAIN=127.0.0.1 \
+  --env DEFAULT_DOMAIN=localhost \
+  --env ORG_NAME="Tuleap" \
   jariasl/tuleap
 ```
 
@@ -17,30 +18,9 @@ docker exec tuleap /bin/bash -c "cat /root/.tuleap_passwd"
 ```sh
 docker run --detach --name tuleap \
   -p 80:80 -p 443:443 \
-  --env DEFAULT_DOMAIN=127.0.0.1 \
-  -v tuleap-conf-tuleap:/etc/tuleap \
-  -v tuleap-data-root:/root \
-  -v tuleap-data-home:/home \
-  -v tuleap-data-tuleap:/var/lib/tuleap \
-  -v tuleap-data-gitolite:/var/lib/gitolite \
-  -v tuleap-data-mailman:/var/lib/mailman \
-  -v tuleap-data-mysql:/var/lib/mysql \
-  jariasl/tuleap
-```
-
-OR
-
-```sh
-docker run --detach --name tuleap \
-  -p 80:80 -p 443:443 \
-  --env DEFAULT_DOMAIN=127.0.0.1 \
-  -v tuleap-conf:/etc/tuleap \
-  -v tuleap-data:/root \
-  -v tuleap-data:/home \
-  -v tuleap-data:/var/lib/tuleap \
-  -v tuleap-data:/var/lib/gitolite \
-  -v tuleap-data:/var/lib/mailman \
-  -v tuleap-data:/var/lib/mysql \
+  --env DEFAULT_DOMAIN=localhost \
+  --env ORG_NAME="Tuleap" \
+  -v tuleap-data:/data \
   jariasl/tuleap
 ```
 
--- docker-wrapper.sh
+++ docker-wrapper.sh
@@ -1,24 +1,75 @@
 #!/bin/bash
 
+function create-data-dirs {
+        mkdir -p /data/etc/tuleap
+        mkdir -p /data/etc/ssh
+        mkdir -p /data/home
+        mkdir -p /data/var/lib/tuleap
+        mkdir -p /data/var/lib/gitolite
+        mkdir -p /data/var/lib/mailman
+        mkdir -p /data/var/lib/mysql
+        mkdir -p /data/root
+}
+
+function move-data-dirs {
+        mv /etc/tuleap /data/etc/
+        mv /etc/ssh /data/
+        mv /home /data/
+        mv /var/lib/tuleap /data/var/lib/
+        mv /var/lib/gitolite /data/var/lib/
+        mv /var/lib/mailman /data/var/lib/
+        mv /var/lib/mysql /data/var/lib/
+        mv /root /data/root && chmod 700 /data/
+}
+
+function create-data-symlinks {
+	ln -s /data/etc/tuleap /etc/tuleap
+        ln -s /data/etc/ssh /etc/ssh
+        ln -s /data/home /home
+        ln -s /data/var/lib/tuleap /var/lib/tuleap
+        ln -s /data/var/lib/gitolite /var/lib/gitolite
+        ln -s /data/var/lib/mailman /var/lib/mailman
+        ln -s /data/var/lib/mysql /var/lib/mysql
+        ln -s /data/root /root
+}
+
+function remove-data-dirs {
+        rm -rf /etc/tuleap
+        rm -rf /etc/ssh 
+        rm -rf /home 
+        rm -rf /var/lib/tuleap 
+        rm -rf /var/lib/gitolite 
+        rm -rf /var/lib/mailman 
+        rm -rf /var/lib/mysql 
+        rm -rf /root
+}
+
 if [[ ! -e /etc/tuleap/conf/local.inc ]]; then
-	echo -e "$DEFAULT_DOMAIN\n$ORG_NAME" | /usr/share/tuleap-install/setup.sh
+	/usr/share/tuleap-install/setup.sh --disable-domain-name-check --sys-default-domain=$DEFAULT_DOMAIN --sys-org-name=$DEFAULT_DOMAIN --sys-long-org-name=$DEFAULT_DOMAIN
 	# Post configuration
 	#yum install -y system-config-firewall-base && yum clean all
 	#lokkit -s https -s http -s ssh
-else
-	#/sbin/service xinetd restart
-	/sbin/service vsftpd restart
-	#/sbin/service named restart
-	#/sbin/service mailman restart </dev/null >/dev/null 2>/dev/null &
-	/sbin/service mysqld restart
-	/usr/lib/forgeupgrade/bin/forgeupgrade --config=/etc/tuleap/forgeupgrade/config.ini update # Update
-	/sbin/service httpd restart
-	/sbin/service crond restart
-	#/sbin/service nscd restart
-	#/sbin/service munin-node restart
-	#/sbin/service nscd restart
-	#/sbin/service openfire restart
+	service --status-all | grep running... | awk '{print $1;}' | xargs -l -I{} service {} stop
+	if [[ ! -e /data/etc/tuleap/conf/local.inc ]]; then
+		create-data-dirs
+	        move-data-dirs
+	else
+		remove-data-dirs
+	fi
 fi
+create-data-symlinks
+/usr/lib/forgeupgrade/bin/forgeupgrade --config=/etc/tuleap/forgeupgrade/config.ini update # Update
+#/sbin/service xinetd restart
+/sbin/service vsftpd restart
+#/sbin/service named restart
+/sbin/service mailman restart </dev/null >/dev/null 2>/dev/null &
+/sbin/service mysqld restart
+/sbin/service httpd restart
+/sbin/service crond restart
+#/sbin/service nscd restart
+#/sbin/service munin-node restart
+#/sbin/service nscd restart
+#/sbin/service openfire restart
 
 # Tail all logs
 tail -f /var/log/httpd/*
